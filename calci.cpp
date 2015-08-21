542d319e9b14b3b0b6c438d2e7b27b55e7cbf14a
Abhishikta7 <Abhishikta7@users.noreply.github.com>
2015-08-21 14:34:33 +0530

    Update calci.cpp

--- calci.cpp
+++ calci.cpp
@@ -1,2 +1,3 @@
-This is a file which is named Calci. Its extension indicates its a c++ program.
+This is a file which is named Calci.
 Im looking forward to make a calculator which has scientific calculator options too.
+Hi are you a mathematician who needs a calculator? Or an acountant?
