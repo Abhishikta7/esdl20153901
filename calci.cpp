256bd8594a3d1850e8ecf8addb34b43ffdc4b9e6
Abhishikta7 <Abhishikta7@users.noreply.github.com>
2015-08-21 14:35:28 +0530

    Edited version

--- calci.cpp
+++ calci.cpp
@@ -1,3 +1,3 @@
-This is a file which is named Calci.
+This is a file which is named Calci. hi all
 Im looking forward to make a calculator which has scientific calculator options too.
 Hi are you a mathematician who needs a calculator? Or an acountant?
